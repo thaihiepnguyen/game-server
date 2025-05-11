1e4978dd2c2bf7b511ea2eceebab4b1fdcfc1045
hieon <thaihiep232002@gmail.com>
2025-05-11 11:45:02 +0700

    feat: add dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <memory>
 #include "core/test.cpp"
-
+#include <fmt/core.h>
+#include <fmt/color.h>
 
 
 int main() {
-    Animal* a = new Animal();
-
-    a->park();
+    fmt::print("Hello, {}!\n", "fmt");
+    fmt::print(fmt::emphasis::bold | fg(fmt::color::spring_green),
+               "Formatted number: {:08}\n", 42);
 
     return 0;
 }
